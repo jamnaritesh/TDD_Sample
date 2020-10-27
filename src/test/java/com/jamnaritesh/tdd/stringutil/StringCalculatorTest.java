974d919b1e44bc3802ef4efd3f3e44b88d473e06
Ritesh Sharma <riteshsharma@Riteshs-MacBook-Pro.local>
2020-10-27 15:13:18 +0530

    Throw exception when negative numbers in input

--- src/test/java/com/jamnaritesh/tdd/stringutil/StringCalculatorTest.java
+++ src/test/java/com/jamnaritesh/tdd/stringutil/StringCalculatorTest.java
@@ -6,27 +6,32 @@ public class StringCalculatorTest {
     StringCalculator calculator = new StringCalculator();
 
     @Test
-    public void whenEmptyStringThenReturnZero(){
+    public void whenEmptyStringThenReturnZero() throws Exception {
         Assert.assertEquals(0, calculator.add(""));
     }
 
     @Test
-    public  void whenSingleDigitThenReturnIdentity(){
+    public  void whenSingleDigitThenReturnIdentity() throws Exception {
         Assert.assertEquals(1,calculator.add("1"));
     }
 
     @Test
-    public  void whenDoubleDigitThenReturnAddition(){
+    public  void whenDoubleDigitThenReturnAddition() throws Exception {
         Assert.assertEquals(3,calculator.add("1,2"));
     }
 
     @Test
-    public void whenNewLineThenConsiderAsComma(){
+    public void whenNewLineThenConsiderAsComma() throws Exception {
         Assert.assertEquals(6,calculator.add("1\n2,3"));
     }
 
     @Test
-    public void AcceptDelimiter(){
+    public void AcceptDelimiter() throws Exception {
         Assert.assertEquals(3,calculator.add("//;\\n1;2"));
+    }
+
+    @Test(expected = Exception.class)
+    public void throwExceptionWhenInputHasNegativeNumbers() throws Exception {
+        calculator.add("-1,2,3");
     }
 }
