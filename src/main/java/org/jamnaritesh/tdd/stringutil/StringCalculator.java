c8745a3f12ea27369aeff5550127a9416543dc4e
Ritesh Sharma <riteshsharma@Riteshs-MacBook-Pro.local>
2020-10-27 14:51:06 +0530

    Added impkementation for new line input

--- src/main/java/org/jamnaritesh/tdd/stringutil/StringCalculator.java
+++ src/main/java/org/jamnaritesh/tdd/stringutil/StringCalculator.java
@@ -1,12 +1,19 @@
 package org.jamnaritesh.tdd.stringutil;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
 
 public class StringCalculator {
     public int add(String number){
         if(number.equals(""))
         return 0;
-        int sum = Arrays.stream(Arrays.stream(number.split(",")).mapToInt(Integer::parseInt).toArray()).sum();
+        String sanitized = replaceNewLine(number);
+        System.out.println(sanitized);
+        int sum = Arrays.stream(Arrays.stream(sanitized.split(",")).mapToInt(Integer::parseInt).toArray()).sum();
         return sum;
+    }
+
+    private String replaceNewLine(String str){
+        return str.replaceAll(Matcher.quoteReplacement("\n"),",");
     }
 }
