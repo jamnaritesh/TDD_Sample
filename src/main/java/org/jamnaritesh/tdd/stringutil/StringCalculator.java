76b57124ded77a74b31021a422bce886585642fc
Ritesh Sharma <riteshsharma@Riteshs-MacBook-Pro.local>
2020-10-27 13:05:40 +0530

    Added testcases for addition

--- src/main/java/org/jamnaritesh/tdd/stringutil/StringCalculator.java
+++ src/main/java/org/jamnaritesh/tdd/stringutil/StringCalculator.java
@@ -1,9 +1,12 @@
 package org.jamnaritesh.tdd.stringutil;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int add(String number){
         if(number.equals(""))
         return 0;
-        return 0;
+        int sum = Arrays.stream(Arrays.stream(number.split(",")).mapToInt(Integer::parseInt).toArray()).sum();
+        return sum;
     }
 }
--- src/test/java/com/jamnaritesh/tdd/stringutil/StringCalculatorTest.java
+++ src/test/java/com/jamnaritesh/tdd/stringutil/StringCalculatorTest.java
@@ -3,9 +3,20 @@ import org.jamnaritesh.tdd.stringutil.StringCalculator;
 import org.junit.Assert;
 import org.junit.Test;
 public class StringCalculatorTest {
+    StringCalculator calculator = new StringCalculator();
+
     @Test
     public void whenEmptyStringThenReturnZero(){
-        StringCalculator calculator = new StringCalculator();
         Assert.assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public  void whenSingleDigitThenReturnIdentity(){
+        Assert.assertEquals(1,calculator.add("1"));
+    }
+
+    @Test
+    public  void whenDoubleDigitThenReturnAddition(){
+        Assert.assertEquals(3,calculator.add("1,2"));
     }
 }
